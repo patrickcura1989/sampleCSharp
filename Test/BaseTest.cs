c3b27696bdec259146e022763f4e57b505fae46b
LEGION-PC <patrick_cura1989@yahoo.com>
2023-07-26 22:14:07 +1200

    Add drivers

--- Test/BaseTest.cs
+++ Test/BaseTest.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
 
 namespace Test.UnitTest
 {
@@ -13,7 +15,8 @@ namespace Test.UnitTest
         [SetUp]
         public void SetUp()
         {
-            Driver = new ChromeDriver();
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            Driver = new ChromeDriver(path + @"\Drivers\");
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl(_baseUrl);
         }
